6afe6b38ca113d4cba78065a3070c6efa8073bc6
To Chan <ctchan3@Chaks-MacBook-Air.local>
2013-09-15 15:03:06 +0800

    Initial Commit

---
+++ Air Hockey/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Air_HockeyAppController.h
+//  Air Hockey
+//
+//  Created by To Chan on 15/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Air Hockey/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Air_HockeyAppController.h
+//  Air Hockey
+//
+//  Created by To Chan on 15/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
